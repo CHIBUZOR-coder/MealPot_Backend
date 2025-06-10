76d099b42eade14d1f82a66e5941d719c316f011
Chibuzor <chibuzormekalam@gmail.com>
2025-06-10 13:50:11 +0100

    removed pool.end from conection to data base

--- db.js
+++ db.js
@@ -1,4 +1,29 @@
+// import pg from "pg";
+// import dotenv from "dotenv";
+// dotenv.config();
+// const pool = new pg.Pool({
+//   user: "postgres",
+//   password: process.env.DB_PASSWORD,
+//   host: "localhost",
+//   port: 5432,
+//   database: "mealpot",
+// });
 
+// //named export
+// // export {pool}
+// // Test the database connection with try-catch
+// const testDbConnection = async () => {
+//   try {
+//     await pool.connect();
+//     console.log("Connected to the database successfully.");
+//   } catch (err) {
+//     console.error("Error connecting to the database:", err);
+//   }
+// };
+
+// // Call the test function
+// testDbConnection();
+// //default export
 // export default pool;
 import pg from "pg";
 import dotenv from "dotenv";
@@ -14,19 +39,17 @@ const pool = new pg.Pool({
 });
 
 // Test the database connection with try-catch
-const DbConnection = async () => {
+const testDbConnection = async () => {
   try {
     await pool.connect();
     console.log("Connected to the database successfully.");
   } catch (err) {
     console.error("Error connecting to the database:", err);
-  } finally {
-    await pool.end(); // Close the pool to prevent hanging connections
-  }
+  } 
 };
 
 // Call the test function
-DbConnection();
+testDbConnection();
 
 // Default export
 
